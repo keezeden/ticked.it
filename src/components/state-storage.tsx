0c0b32285a2ff0d9555b7d34f392cccd12d687cc
Key-github <keezeden@gmail.com>
2024-11-10 15:37:15 +1300

    remove useless hook

--- src/components/state-storage.tsx
+++ src/components/state-storage.tsx
@@ -1,9 +1,39 @@
 "use client";
 
-import { useStorage } from "@/hooks/sync";
+import { useData } from "./data-provider";
+import { useEffect } from "react";
+
+const STORAGE_KEY = "ticked.it/tasks";
+const DEFAULT_DATA: Task[] = [
+  {
+    id: 1,
+    task: "Clean Room",
+  },
+  {
+    id: 2,
+    task: "Wash Dishes",
+  },
+  {
+    id: 3,
+    task: "Do Laundry",
+  },
+];
 
 export const StateStorage = () => {
-  useStorage();
+  const { data, setData } = useData();
+
+  const save = (data: any) => localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+
+  const load = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || JSON.stringify(DEFAULT_DATA));
+
+  useEffect(() => {
+    const data = load();
+    setData(data);
+  }, []);
+
+  useEffect(() => {
+    save(data);
+  }, [data]);
 
   return null;
 };
